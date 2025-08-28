fc3a19e77aa55958662d215c5420e4fb2fbc94a2
Bai1026 <1132abc30@gmail.com>
2025-08-28 17:17:58 +0800

    0828

---
+++ Basic_Python/0828.py
@@ -0,0 +1,73 @@
+# age = 25
+# print(type(age))
+# print(age)
+
+# age = '25'
+# print(type(age))
+# print(age)
+
+# age = 30
+# print(age)
+
+# for _ in range(3):
+    # age += 1
+    # print(age)
+
+# a = 10
+# b = 20
+# print(a,b)
+# if a == b:
+#     print("a 等於 b")
+# else:
+#     print("a 不等於 b")
+# print()
+
+# a = b
+# print(a,b)
+# if a == b:
+#     print("a 等於 b")
+# else:
+#     print("a 不等於 b")
+# print()
+
+# print('fuck u la')
+# print()
+# print('fuck \nu \nla')
+
+# a = 20
+# b = '你好'
+# b = 1
+# print(b + '白痴')
+
+
+# print('alkjflkaslkfjlsdkajflksdajfljsdlfjlkasdjflkjalkjflkasjflkjsdalfjlksdajfklsadjlfkjsalkfjlskdajfklsadjflkjsadlkfjlkasdjflksdajflkajslkfjaslkjdkljsdalkjflkasjflkjsadlkfjlkasdjflksdjflksdjlkfsj')
+# print('='*100)
+# print('lakjdflkjalkfjsldkflkjflkasjflkjsdalfjlksdajfklsadjlfkjsalkfjlskdajfklsadjflkjsadlkfjlkasdjflksdajflkajslkfjaslkjdkljsdalkjflkasjflkjsadlkfjlkasdjflksdjflksdjlkfsj')
+
+# # end 參數：控制結尾字元
+# print("載入中", end="")
+# for i in range(3):
+#     print(".", end="")
+# print(" 完成!")  # 輸出: 載入中... 完成!
+
+# # sep 參數：控制分隔字元
+# print("蘋果", "香蕉", "橘子", sep=" | ")  # 輸出: 蘋果 | 香蕉 | 橘子
+
+
+# print()
+# # end 參數：控制結尾字元
+# print("載入中", end="")
+# for i in range(100):
+#     print(".", end="")
+# print(" 完成!")  # 輸出: 載入中... 完成!
+
+# # sep 參數：控制分隔字元
+# print("蘋果", "香蕉", "橘子", sep=" | ")  # 輸出: 蘋果 | 香蕉 | 橘子
+
+
+# # file 參數：輸出到檔案
+# with open("log.txt", "w") as file:
+#     print("這是日誌訊息", file=file)  # 輸出到檔案而不是終端機
+
+for i in range(5):
+    print(i)
